86a162390ccd4a1ac11bf4a2c54079a38f37b579
Jorge Marquez <83016935+jorgemarquez2222@users.noreply.github.com>
2022-08-12 19:08:08 -0400

    add backend request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
+async function getUsers() {
+  const options = { method: 'GET', url: 'http://localhost:3000/users' };
+  // axios.request(options).then(function (response) {
+  //   console.log("aqui la respuesta de la llamada",response.data);
+  //   return response.data
+  // }).catch(function (error) {
+  //   console.error(error);
+  // });
+  const response = await axios.request(options)
+  return response.data
+}
 
 function App() {
   const data = [
@@ -16,26 +28,50 @@ function App() {
     }
   ]
   const [count, setCount] = useState(0); // hook
+  const [dataBack, setDataBack] = useState([]); // hook
   useEffect(() => {
     document.title = `You clicked ${count} times`;
-  });
+    getUsers().then((data)=>{
+      console.log("aqui es data", data)
+      setDataBack(data)
+    })
+  }, []);
   const element = (
     <ul>
-     { data.length &&
-      data.map((item, index) => {
-        return <li key={index}> {item.name} </li>
-      })
-     }
+      {data.length &&
+        data.map((item, index) => {
+          return <li key={index}> {item.name} </li>
+        })
+      }
     </ul>
   )
+  const listData = (
+    <ul>
+      {
+        dataBack.map((item, index)=>{
+          return <li key={index}>{item.name}</li>
+        })
+      }
+    </ul>
+  )
+  
   return (
     <div className="nombreDeLaClase">
       <h2>Hola fran</h2>
       {element}
+      {listData}
       <p>tu cliqueaste {count} veces</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
+      <button onClick={() => {
+        getUsers().then((data)=>{
+          setDataBack(data)
+        })
+      }}>
+        Click al backend
+      </button>
+     
     </div>
   )
 }
