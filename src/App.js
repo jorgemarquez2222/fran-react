2f725a33952d0663eb2735cae2c7a63bc1f35a7a
siglasconexion <109320123+siglasconexion@users.noreply.github.com>
2022-08-12 18:01:03 -0400

    create table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-
 function App() {
   const data = [
     {
@@ -20,16 +19,35 @@ function App() {
     document.title = `You clicked ${count} times`;
   });
   const element = (
-    <ul>
-     { data.length &&
-      data.map((item, index) => {
-        return <li key={index}> {item.name} </li>
-      })
-     }
-    </ul>
-  )
+    //<ul>
+    // { data.length &&
+    //  data.map((item, index) => {
+    //    return <li key={index}> {item.name} </li>
+    //  })
+    // }
+    //</ul>
+      // return <li key={index}> {item.name} </li>
+      <table border='='>
+      <tr>
+        <th>indice</th>
+        <th>Nombre</th>
+        <th>Edad</th>
+     </tr>
+    { data.length &&
+     data.map((item, index) => {
+      return (<tr>
+          <td> {index}</td>
+          <td>{item.name}</td>
+          <td>{item.edad}</td>
+      </tr>)
+     })
+    }
+   </table>
+
+)
   return (
     <div className="nombreDeLaClase">
+<p>prueba de esta etiqueta</p>
       <h2>Hola fran</h2>
       {element}
       <p>tu cliqueaste {count} veces</p>
