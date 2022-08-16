cb9ae4d6d2b02b924bdabf18cc7b043484d72113
Jorge Marquez <83016935+jorgemarquez2222@users.noreply.github.com>
2022-08-16 19:22:06 -0400

    Add todoList function

--- src/App.js
+++ src/App.js
@@ -14,6 +14,20 @@ async function getUsers() {
   return response.data
 }
 
+async function deleteUser(info) {
+  const options = { method: 'PUT',
+  data: JSON.stringify(info),
+  url: 'http://localhost:3000/user' };
+  // axios.request(options).then(function (response) {
+  //   console.log("aqui la respuesta de la llamada",response.data);
+  //   return response.data
+  // }).catch(function (error) {
+  //   console.error(error);
+  // });
+  const response = await axios.request(options)
+  return response.data
+}
+
 function App() {
   const data = [
     {
@@ -29,6 +43,8 @@ function App() {
   ]
   const [count, setCount] = useState(0); // hook
   const [dataBack, setDataBack] = useState([]); // hook
+  const [ userInput, setUserInput ] = useState('');
+
   useEffect(() => {
     document.title = `You clicked ${count} times`;
     getUsers().then((data)=>{
@@ -54,12 +70,28 @@ function App() {
       }
     </ul>
   )
-  
+  const handleChange = (e) => {
+    setUserInput(e.currentTarget.value)
+}
   return (
     <div className="nombreDeLaClase">
       <h2>Hola fran</h2>
       {element}
+     
       {listData}
+      {userInput}
+      <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
+      <button onClick={() => {
+        const copia = [...dataBack]
+        copia.push({
+          name: userInput,
+          edad: 50,
+        })
+        setDataBack(copia)
+        console.log("Ejecutando boton")
+      }}>
+        Click me
+      </button>
       <p>tu cliqueaste {count} veces</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
