01efe9df98c7bd762e66acf731a6223c8e9d9e08
Jorge Marquez <83016935+jorgemarquez2222@users.noreply.github.com>
2022-08-17 16:58:19 -0400

    Add button delete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.css'
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
@@ -14,17 +14,18 @@ async function getUsers() {
   return response.data
 }
 
-async function deleteUser(info) {
-  const options = { method: 'PUT',
-  data: JSON.stringify(info),
-  url: 'http://localhost:3000/user' };
+async function deleteUser(person) {
+  const options = { 
+    data: { 
+      id: person.id
+  }}
   // axios.request(options).then(function (response) {
   //   console.log("aqui la respuesta de la llamada",response.data);
   //   return response.data
   // }).catch(function (error) {
   //   console.error(error);
   // });
-  const response = await axios.request(options)
+  const response = await axios.delete('http://localhost:3000/user', options)
   return response.data
 }
 
@@ -43,7 +44,7 @@ function App() {
   ]
   const [count, setCount] = useState(0); // hook
   const [dataBack, setDataBack] = useState([]); // hook
-  const [ userInput, setUserInput ] = useState('');
+  const [ userInput, setUserInput ] = useState({name:""});
 
   useEffect(() => {
     document.title = `You clicked ${count} times`;
@@ -61,37 +62,43 @@ function App() {
       }
     </ul>
   )
+  const clickUser = async (item) =>{
+    console.log('Holaaa', item)
+    console.log(await deleteUser({ id: item.id}))
+    const tmp = [...dataBack]
+    const f = tmp.filter((i) => i.name !== item.name)
+    setDataBack(f)
+  }
   const listData = (
     <ul>
       {
         dataBack.map((item, index)=>{
-          return <li key={index}>{item.name}</li>
+          return <li key={index} 
+           >{item.name}
+           <button onClick={() => clickUser(item)}>
+        X
+      </button>
+           </li>
         })
       }
     </ul>
   )
   const handleChange = (e) => {
-    setUserInput(e.currentTarget.value)
+    setUserInput({ ...userInput, name: e.currentTarget.value })
+
+    console.log('userInput', userInput)
+    e.preventDefault();
+
 }
   return (
     <div className="nombreDeLaClase">
       <h2>Hola fran</h2>
       {element}
-     
       {listData}
-      {userInput}
-      <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
-      <button onClick={() => {
-        const copia = [...dataBack]
-        copia.push({
-          name: userInput,
-          edad: 50,
-        })
-        setDataBack(copia)
-        console.log("Ejecutando boton")
-      }}>
-        Click me
-      </button>
+
+      {userInput.name}
+      <input value={userInput.name} type="text" onChange={handleChange} />
+
       <p>tu cliqueaste {count} veces</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
