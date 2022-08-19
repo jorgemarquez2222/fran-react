58280784a3947f12a0e233a207e305ff8e7f48e4
Jorge Marquez <83016935+jorgemarquez2222@users.noreply.github.com>
2022-08-19 15:40:28 -0400

    name app changed

--- src/App.js
+++ src/App.js
@@ -4,27 +4,16 @@ import axios from "axios";
 
 async function getUsers() {
   const options = { method: 'GET', url: 'http://localhost:3000/users' };
-  // axios.request(options).then(function (response) {
-  //   console.log("aqui la respuesta de la llamada",response.data);
-  //   return response.data
-  // }).catch(function (error) {
-  //   console.error(error);
-  // });
   const response = await axios.request(options)
   return response.data
 }
 
 async function deleteUser(person) {
-  const options = { 
-    data: { 
+  const options = {
+    data: {
       id: person.id
-  }}
-  // axios.request(options).then(function (response) {
-  //   console.log("aqui la respuesta de la llamada",response.data);
-  //   return response.data
-  // }).catch(function (error) {
-  //   console.error(error);
-  // });
+    }
+  }
   const response = await axios.delete('http://localhost:3000/user', options)
   return response.data
 }
@@ -44,10 +33,11 @@ function App() {
   ]
   const [count, setCount] = useState(0); // hook
   const [dataBack, setDataBack] = useState([]); // hook
-  const [ userInput, setUserInput ] = useState({name:""});
+  const [ userInput, setUserInput ] = useState('');
 
   useEffect(() => {
     document.title = `You clicked ${count} times`;
+    // const data = await getUsers() es lo mismo que la linea de abajo (no habilitar)
     getUsers().then((data)=>{
       console.log("aqui es data", data)
       setDataBack(data)
@@ -62,43 +52,47 @@ function App() {
       }
     </ul>
   )
-  const clickUser = async (item) =>{
-    console.log('Holaaa', item)
-    console.log(await deleteUser({ id: item.id}))
+  const clickUser = async (item) => {
+    console.log("Hola fran", item)
     const tmp = [...dataBack]
-    const f = tmp.filter((i) => i.name !== item.name)
+    const f = tmp.filter((i)=> i.name !== item.name )
     setDataBack(f)
+    await deleteUser({ id: item.id })
   }
   const listData = (
     <ul>
       {
         dataBack.map((item, index)=>{
-          return <li key={index} 
-           >{item.name}
-           <button onClick={() => clickUser(item)}>
-        X
-      </button>
-           </li>
+          return <li key={index}>
+            {item.name}
+            <button onClick={()=> clickUser(item)} >X</button>
+            </li>
         })
       }
     </ul>
   )
-  const handleChange = (e) => {
-    setUserInput({ ...userInput, name: e.currentTarget.value })
-
-    console.log('userInput', userInput)
-    e.preventDefault();
-
+  const cambioDeInput = (e) => {
+    setUserInput(e.currentTarget.value)
 }
   return (
     <div className="nombreDeLaClase">
       <h2>Hola fran</h2>
       {element}
+     
       {listData}
-
-      {userInput.name}
-      <input value={userInput.name} type="text" onChange={handleChange} />
-
+      {userInput}<br/>
+      <input value={userInput} type="text" onChange={cambioDeInput} placeholder="Enter task..."/>
+      <button onClick={() => {
+        const copia = [...dataBack]
+        copia.push({
+          name: userInput,
+          edad: 50,
+        })
+        setDataBack(copia)
+        console.log("Ejecutando boton")
+      }}>
+        Click me
+      </button>
       <p>tu cliqueaste {count} veces</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
